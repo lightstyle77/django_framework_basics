97b51e0d131375143b7e4243b3c91060efe8aade
lightstyle77 <lightstyle54@mail.ru>
2021-05-12 19:46:29 +0300

    completed

---
+++ geekshop/mainapp/migrations/0002_productcategory_is_active.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('mainapp', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='productcategory',
+            name='is_active',
+            field=models.BooleanField(default=True, verbose_name='активная'),
+        ),
+    ]
